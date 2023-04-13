3f1fe1398cb7dbce59d2d070fc5fee262cb161fd
Tim Morgner <tim@timmorgner.de>
2023-04-13 12:56:59 +0200

    Aufteilen in Funktionen

--- app.py
+++ app.py
@@ -5,41 +5,55 @@ import csv
 import random
 import threading
 
+print("Server started")
+
 def read_csv_file(file_path):
-    data = []
+    kuerzel = []
+    wert = []
     with open(file_path, mode='r') as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
-            data.append(row)
-    return data
+            kuerzel.append(row['Kuerzel'])
+            wert.append(float(row['Wert']))
+    return kuerzel, wert
 
 file_path = 'vs_p1_wertpapiere.csv'
-data = read_csv_file(file_path)
+kuerzel, wert = read_csv_file(file_path)
 
-print("Started")
+print("Loaded {} stocks".format(len(kuerzel)))
 
-localIP = ""
-localPort = int(os.environ.get("PORT",12345))
-bufferSize = 1024
-
-msgFromServer = "Hello UDP Client"
-bytesToSend = str.encode(msgFromServer)
+LOCAL_IP = ""
+LOCAL_PORT = int(os.environ.get("PORT",12345))
+BUFFER_SIZE = 1024
 
 # Create a datagram socket
-print("Setting up socket with IP: {} and Port: {}".format(localIP, localPort))
+print("Setting up socket with IP: {} and Port: {}".format(LOCAL_IP, LOCAL_PORT))
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 # Bind to address and ip
-UDPServerSocket.bind((localIP, localPort))
+UDPServerSocket.bind((LOCAL_IP, LOCAL_PORT))
 print("UDP server up and listening")
 
 # A dictionary to keep track of connected clients and their addresses
 connected_clients = {}
 
+def send_all_prices(address):
+    for i in range(len(kuerzel)):
+        send_message(kuerzel[i] + ";" + str(wert[i]), address)
+
+def send_message(message, address):
+    bytesToSend = str.encode(message)
+    UDPServerSocket.sendto(bytesToSend, address)
+
+def broadcast_message(message):
+    for address in connected_clients.keys():
+        send_message(message, address)
+
+
 # A function that listens for incoming datagrams and sends price changes to connected clients
 def listen_for_datagrams():
     # Receive datagram and get the client's address
-    bytes_address_pair = UDPServerSocket.recvfrom(bufferSize)
+    bytes_address_pair = UDPServerSocket.recvfrom(BUFFER_SIZE)
     message = bytes_address_pair[0]
     address = bytes_address_pair[1]
     while True:
@@ -50,9 +64,9 @@ def listen_for_datagrams():
             print("New client connected: {}".format(address))
 
         # Send the current prices to the client
-        #for row in data:
-         #   bytesToSend = str.encode(row['Kuerzel'] + ";" + str(row['Wert']))
-          #  UDPServerSocket.sendto(bytesToSend, address)
+        if message == b'all':
+            print("Sending all prices to {}".format(address))
+            send_all_prices(address)
 
     # Close the socket when the thread is finished
     UDPServerSocket.close()
@@ -61,17 +75,17 @@ def listen_for_datagrams():
 def change_prices():
     while True:
         # Generate a random price change and apply it to a random stock
-        wId = random.randint(0, len(data) - 1)
+        wId = random.randint(0, len(kuerzel) - 1)
         priceChange = random.randint(-10, 10)
-        data[wId]['Wert'] = float(data[wId]['Wert']) + priceChange
-
-        # Send the new prices to all connected clients
-        for address in connected_clients.keys():
-            bytesToSend = str.encode(data[wId]['Kuerzel'] + ";" + str(data[wId]['Wert']))
-            UDPServerSocket.sendto(bytesToSend, address)
+        if priceChange != 0:
+            wert[wId] = float(wert[wId]) + priceChange
+            print("New price for {}: {} (change: {})".format(kuerzel[wId], wert[wId], priceChange))
+            broadcast_message(kuerzel[wId] + ";" + str(wert[wId]))
 
         # Wait for a random amount of time before generating the next price change
-        time.sleep(random.uniform(0.5, 2.0))
+        waitTime = random.uniform(2.0, 10.0)
+        print("Waiting for {} seconds".format(waitTime))
+        time.sleep(waitTime)
 
 # Start the thread that listens for incoming datagrams
 t1 = threading.Thread(target=listen_for_datagrams)
